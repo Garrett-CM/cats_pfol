d9abc1d207f00c93ddd07464f1986f80949524a6
Garry <GarrettCalebMiller@gmail.com>
2026-05-01 22:21:26 -0500

    added quit

--- game.py
+++ game.py
@@ -12,11 +12,11 @@ TITLE         = "Cats, Pocket Full of Litter"
 FPS           = 60
 
 # grabbed a gothic color palette (R, G, B)
-DEEP_PURPLE = (40,  0,   60)
-BLOOD_RED   = (139, 0,   0)
+DEEP_PURPLE = (40, 0, 60)
+BLOOD_RED   = (139, 0, 0)
 PALE_GOLD   = (200, 180, 100)
 GHOST_WHITE = (220, 220, 240)
-DARK_GREY   = (30,  30,  30)
+DARK_GREY   = (30, 30, 30)
 
 #start game & window size
 pygame.init()
@@ -27,7 +27,14 @@ pygame.display.set_caption(TITLE)
 running = True
 while running:
     #handle event
+        #this handles closing the app
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
     #update
     screen.fill(DARK_GREY)
     #draw
     pygame.display.flip()
+
+pygame.quit
